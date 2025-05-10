e9dccb3bf737e70c11331a6642c126cf0cb06451
Kienan <69349336+KienanUnique@users.noreply.github.com>
2025-05-10 12:48:08 +0500

    PIR-14 fix non binded view initialize and add sprite rotator module

---
+++ Assets/Scripts/Game/Services/CameraHolder/ICameraHolderService.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Game.Services.CameraHolder
+{
+    public interface ICameraHolderService
+    {
+        Vector3 CameraPosition { get; }
+    }
+}
---
+++ Assets/Scripts/Game/Views/TalkableCharacter/SpriteRotatorModule.cs
@@ -0,0 +1,35 @@
+﻿using Game.Core;
+using Game.Services.CameraHolder;
+using R3;
+using UnityEngine;
+using Zenject;
+
+namespace Game.Views.TalkableCharacter
+{
+    public class SpriteRotatorModule : AModule
+    {
+        [SerializeField] private Transform _objectToRotate;
+        
+        [Inject] private ICameraHolderService _cameraHolderService;
+
+        public override void Initialize()
+        {
+            Observable.EveryUpdate(UnityFrameProvider.Update).Subscribe(_ => OnUpdate()).AddTo(this);
+        }
+
+        private void OnUpdate()
+        {
+            var cameraPosition = _cameraHolderService.CameraPosition;
+            var toCamera = cameraPosition - _objectToRotate.position;
+            
+            var targetRotation = Quaternion.LookRotation(-toCamera);
+            
+            var euler = targetRotation.eulerAngles;
+            var selfRotation = _objectToRotate.rotation;
+            euler.x = selfRotation.eulerAngles.x;
+            euler.z = transform.rotation.eulerAngles.z;
+        
+            _objectToRotate.rotation = Quaternion.Euler(euler);
+        }
+    }
+}
