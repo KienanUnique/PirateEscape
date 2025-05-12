4e1713a8a70a246cdbb16e1c607d02149a8c42f8
Kienan <69349336+KienanUnique@users.noreply.github.com>
2025-05-12 08:31:08 +0500

    PIR-50 add kettle and change timer logic

--- Assets/Scripts/Game/Ui/Gameplay/GameplayWindow.cs
+++ Assets/Scripts/Game/Ui/Gameplay/GameplayWindow.cs
@@ -1,6 +1,7 @@
 ﻿using Game.Ui.Gameplay.ClickInteract;
 using Game.Ui.Gameplay.Grab;
 using Game.Ui.Gameplay.Interaction;
+using Game.Ui.Gameplay.Timer;
 using KoboldUi.Windows;
 using UnityEngine;
 
@@ -11,12 +12,14 @@ namespace Game.Ui.Gameplay
         [SerializeField] private InteractionView _interactionView;
         [SerializeField] private GrabView _grabView;
         [SerializeField] private ClickInteractView _clickInteractView;
+        [SerializeField] private TimerView _timerView;
         
         protected override void AddControllers()
         {
             AddController<InteractionController, InteractionView>(_interactionView);
             AddController<GrabController, GrabView>(_grabView);
             AddController<ClickInteractController, ClickInteractView>(_clickInteractView);
+            AddController<TimerController, TimerView>(_timerView);
         }
     }
 }
